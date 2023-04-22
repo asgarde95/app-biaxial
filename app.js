ae37dbf9627aacc432d003cb860b57a00a76cc8a
Assier Garcia <assiergv@hotmail.com>
2023-04-22 16:31:11 +0200

    Cambios history

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 // Middleware para Vue.js router modo history
 const history = require('connect-history-api-fallback');
 app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'httpdocs')));
 
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), function () {
