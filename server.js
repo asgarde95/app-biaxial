6e35cd5ab5268a728696c82db09ba5251706d028
Assier Garcia <assiergv@hotmail.com>
2023-04-22 14:22:36 +0200

    Instalacion History vue.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Rutas
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+// Middleware para Vue.js router modo history
+const history = require("connect-history-api-fallback");
+app.use(history());
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
