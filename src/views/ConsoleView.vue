01b448f194a83d263395bbe919a37e8df2b1fc8d
Assier Garcia <assiergv@hotmail.com>
2023-05-08 19:02:45 +0200

    s

--- src/views/ConsoleView.vue
+++ src/views/ConsoleView.vue
@@ -1,27 +1,27 @@
 <template>
-    <NavBar></NavBar>
-    <div class="container" style="margin-left=0px; margin-right=0px;">
-        <h1>{{ $t(20) }}</h1>
-        <div class="row">
-            <div style="background-color: white;">
-                <main id="console">
-                  <section id="output" :class="{ newlines:newlines }">
-                    <pre v-for="message in connection.messages" v-html="encodeWithHtml(message)"></pre>
-                  </section>
-                </main>
-            </div>
-          </div>
-            <div class="row">
-                <input class="form-control" type="text" v-model="consoleInput"/>
-                &nbsp;
-                <button class="btn btn-secondary" @click="sendToArduino(consoleInput)" style="width:100px;">{{ $t(23) }}</button>
-            </div>
+  <NavBar></NavBar>
+  <div class="container" style="margin-left=0px; margin-right=0px;">
+    <h1>{{ $t(20) }}</h1>
+    <div class="row">
+      <div style="background-color: white;">
+        <main id="console">
+          <section id="output" :class="{ newlines: newlines }">
+            <pre v-for="message in connection.messages" v-html="encodeWithHtml(message)"></pre>
+          </section>
+        </main>
+      </div>
     </div>
+    <div class="row">
+      <input class="form-control" type="text" v-model="consoleInput" />
+      &nbsp;
+      <button class="btn btn-secondary" @click="sendToArduino(consoleInput)" style="width:100px;">{{ $t(23) }}</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import NavBar from '@/components/NavBar.vue'
-import {decode, encodeWithHtml} from '@/util/asciiEncoder';
+import { decode, encodeWithHtml } from '@/util/asciiEncoder';
 import { useConnectionStore } from '@/store/connection'
 import { ref } from 'vue';
 const connection = useConnectionStore()
@@ -39,30 +39,34 @@ function sendToArduino(consoleInput) {
 </script>
 
 <style scoped>
- #output {
-    background-color: #fcfcfc;
-    min-height: 100%;
-  }
-  #output > pre {
-    word-break: break-all;
-    overflow-wrap: break-word;
-    white-space: pre-wrap;
-    display: inline;
-  }
-  #output > pre > br {
-     display: none;
-  }
-  #output.newlines > pre > br {
-     display: initial;
-  }
-  #prepend, #append {
-    min-width: 2ch;
-    width: fit-content;
-    font-size: 22px;
-  }
+#output {
+  background-color: #fcfcfc;
+  min-height: 100%;
+}
 
-  #console {
-    overflow-y: scroll;
-    height: 500px;
-  }
-</style>
+#output>pre {
+  word-break: break-all;
+  overflow-wrap: break-word;
+  white-space: pre-wrap;
+  display: inline;
+}
+
+#output>pre>br {
+  display: none;
+}
+
+#output.newlines>pre>br {
+  display: initial;
+}
+
+#prepend,
+#append {
+  min-width: 2ch;
+  width: fit-content;
+  font-size: 22px;
+}
+
+#console {
+  overflow-y: scroll;
+  height: 500px;
+}</style>
